206829516a3096b66d02c5f914adcec52e1368b6
Mick <Tim>
2021-10-22 10:05:55 +0700

    test fix error 1

--- app.js
+++ app.js
@@ -72,11 +72,11 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // ต้องการ body ในแบบ raw form (string) ไม่ใช่แบบ json เลยเอา route มาไว้ตรงนี้
-app.post(
-  '/webhook-checkout',
-  express.raw({ type: 'application/json' }), // raw body
-  bookingController.webhookCheckout
-);
+// app.post(
+//   '/webhook-checkout',
+//   express.raw({ type: 'application/json' }), // raw body
+//   bookingController.webhookCheckout
+// );
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // req hit middleware นี้ body จะกลายเป็น json
